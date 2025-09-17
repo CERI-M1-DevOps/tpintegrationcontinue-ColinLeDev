de0485ab81fa37617382070639b9eeccc090797e
Colin <github@col1n.fr>
2025-09-17 12:49:03 +0200

    add test modifiePremierElementAbsent

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -63,6 +63,16 @@ class ListeSimpleTest {
     }
 
     @Test
+    void modifiePremierElementAbsent() {
+        listeATester.ajout(1);
+        listeATester.ajout(2);
+        listeATester.ajout(3);
+        listeATester.modifiePremier(5, 4);
+        assertEquals("ListeSimple(Noeud(1), Noeud(2), Noeud(3))", listeATester.toString());
+        assertEquals(4, listeATester.tete.getSuivant().getSuivant().getElement());
+    }
+
+    @Test
     void modifieTous() {
         listeATester.ajout(1);
         listeATester.ajout(2);
